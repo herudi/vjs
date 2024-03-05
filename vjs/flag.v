6994a7ef7b9560ed8917317f26c3ef79df188d40
herudi <herudi7@gmail.com>
2024-03-05 14:04:24 +0700

    last win

--- vjs/flag.v
+++ vjs/flag.v
@@ -2,13 +2,6 @@ module vjs
 
 #flag -I @VMODROOT/lib
 #flag -lm
-$if tinyc {
-	#flag @VMODROOT/lib/tcc/builtin.c
-	#flag @VMODROOT/lib/tcc/umodti3.c
-	#flag @VMODROOT/lib/tcc/divti3.c
-	#flag @VMODROOT/lib/tcc/udivti3.c
-	#flag @VMODROOT/lib/tcc/udivmodti4.c
-}
 $if linux {
 	#flag @VMODROOT/lib/qjs_linux_x64.a
 } $else $if macos {
@@ -16,6 +9,5 @@ $if linux {
 } $else $if windows {
 	#flag @VMODROOT/lib/qjs_win_x64.a
 }
-
 #include "quickjs-libc.h"
 #include "quickjs.h"
