102f2e5f12cc212803a5a5a4a574df1399cbe788
herudi <herudi7@gmail.com>
2024-07-30 12:54:10 +0700

    EventTarget

--- web/js/event.js
+++ web/js/event.js
@@ -1,19 +1,84 @@
 /* Credit: https://www.npmjs.com/package/event-target-polyfill */
 import { vjs_inspect } from "./util.js";
 
+const inspector_event = (obj) => ({
+  bubbles: obj.bubbles,
+  cancelable: obj.cancelable,
+  composed: obj.composed,
+  currentTarget: obj.currentTarget,
+  returnValue: obj.returnValue,
+  defaultPrevented: obj.defaultPrevented,
+  target: obj.target,
+  timeStamp: obj.timeStamp,
+  type: obj.type,
+});
+
 class Event {
+  #type = void 0;
+  #opts = void 0;
+  __currentTarget = void 0;
+  __target = void 0;
   constructor(type, opts = {}) {
-    this.bubbles = opts.bubbles ?? false;
-    this.cancelable = opts.cancelable ?? false;
-    this.composed = opts.composed ?? false;
-    this.type = type;
+    this.#type = type;
+    this.#opts = opts;
+  }
+
+  get bubbles() {
+    return this.#opts.bubbles ?? false;
+  }
+
+  get cancelable() {
+    return this.#opts.cancelable ?? false;
+  }
+
+  get composed() {
+    return this.#opts.composed ?? false;
+  }
+
+  get type() {
+    return this.#type;
+  }
+
+  get currentTarget() {
+    return this.__currentTarget;
+  }
+
+  get target() {
+    return this.__target;
+  }
+
+  get timeStamp() {
+    return 0;
+  }
+
+  get returnValue() {
+    return true;
+  }
+
+  get defaultPrevented() {
+    return false;
+  }
+
+  [vjs_inspect](format) {
+    return "Event " + format(inspector_event(this));
   }
 }
 globalThis.Event = Event;
 class CustomEvent extends Event {
+  #detail = void 0;
   constructor(type, opts = {}) {
     super(type, opts);
-    this.detail = opts.detail;
+    this.#detail = opts.detail;
+  }
+  get detail() {
+    return this.#detail;
+  }
+
+  [vjs_inspect](format) {
+    return "CustomEvent " + format({
+      ...inspector_event(this),
+      detail: this.detail,
+    });
   }
 }
 globalThis.CustomEvent = CustomEvent;
@@ -21,12 +86,13 @@ class EventTarget {
   __listeners = void 0;
   constructor() {}
   get #listener() {
-    return this.__listeners ??= new Map()
+    return this.__listeners ??= new Map();
   }
   addEventListener(type, listener, options) {
     if (arguments.length < 2) {
       throw new TypeError(
-        "TypeError: Failed to execute 'addEventListener' on 'EventTarget': 2 arguments required, but only " + arguments.length + " present."
+        "TypeError: Failed to execute 'addEventListener' on 'EventTarget': 2 arguments required, but only " +
+          arguments.length + " present.",
       );
     }
     const __listeners = this.#listener;
@@ -42,7 +108,8 @@ class EventTarget {
   removeEventListener(type, listener, _options) {
     if (arguments.length < 2) {
       throw new TypeError(
-        "TypeError: Failed to execute 'addEventListener' on 'EventTarget': 2 arguments required, but only " + arguments.length + " present."
+        "TypeError: Failed to execute 'addEventListener' on 'EventTarget': 2 arguments required, but only " +
+          arguments.length + " present.",
       );
     }
     const __listeners = this.#listener;
@@ -57,9 +124,11 @@ class EventTarget {
   dispatchEvent(event) {
     if (!(event instanceof Event)) {
       throw new TypeError(
-        "Failed to execute 'dispatchEvent' on 'EventTarget': parameter 1 is not of type 'Event'."
+        "Failed to execute 'dispatchEvent' on 'EventTarget': parameter 1 is not of type 'Event'.",
       );
     }
+    event.__currentTarget = this;
+    event.__target = this;
     const type = event.type;
     const __listeners = this.#listener;
     const listenersForType = __listeners.get(type);
